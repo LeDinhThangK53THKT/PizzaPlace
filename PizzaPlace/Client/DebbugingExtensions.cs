a52245487c9502b8a3630f53163921fe8724a032
drixt2001 <thangdrit@gmail.com>
2022-05-31 08:08:55 +0700

    Add project files.

---
+++ PizzaPlace/Client/DebbugingExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace PizzaPlace.Client
+{
+    public static class DebbugingExtensions
+    {
+        private static JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true};
+
+        public static string ToJoin(this object obj) => JsonSerializer.Serialize(obj, options);
+    }
+}
---
+++ PizzaPlace/Client/Pages/InputWatcher.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace PizzaPlace.Client.Pages
+{
+    public class InputWatcher : ComponentBase
+    {
+        private EditContext editcontext = default!;
+        [CascadingParameter]
+        public EditContext EditContext
+        {
+            get => this.editcontext;
+            set
+            {
+                this.editcontext = value;
+                EditContext.OnFieldChanged += async (sender, args) =>
+                {
+                    await FieldChanged.InvokeAsync(args.FieldIdentifier.FieldName);
+                };
+            }
+        }
+
+        [Parameter] 
+        public EventCallback<string> FieldChanged { get; set; }
+        public bool Validate() => EditContext?.Validate() ?? false;
+    }
+}
